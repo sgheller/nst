6c13d54b261fe6c5ce8f68faca51f40b53dc5dcc
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2021-08-27 09:42:58 +0200

    Ajout module phi_product_last_purchase_info

---
+++ phi_product_last_purchase_info/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Phidias : product last purchase info",
+
+    'summary': """
+        Phidias : product last purchase info
+        """,
+
+    'description': """
+        Phidias : product last purchase info
+    """,
+
+    'author': "Phidias",
+    'website': "http://www.phidias.fr",
+    'category': 'Uncategorized',
+    'version': '14.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'purchase',
+    ],
+    "data": [
+        'views/product_view.xml'
+    ]
+}
