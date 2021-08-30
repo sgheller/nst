0f10b5a400c141de048aaa3791bddfac16713845
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2021-08-30 12:26:46 +0200

    [REQ-0000000169] Automatisation appel d’offres pour les fournisseurs référencés dans la fiche article - Gestion des nomenclatures

--- phi_requisition_from_sale_order/wizard/sale_requisition_generate.py
+++ phi_requisition_from_sale_order/wizard/sale_requisition_generate.py
@@ -6,25 +6,51 @@ class SaleRequisitionGenerate(models.TransientModel):
     _name = 'purchase_requisition.requisition.generate'
     _description = 'Generate Requisition From Sale Order'
 
-    lines_ids = fields.Many2many('sale.order.line')
+    product_ids = fields.Many2many('product.product')
 
     @api.model
     def default_get(self, fields):
         res = super(SaleRequisitionGenerate, self).default_get(fields)
         res_ids = self._context.get('active_ids')
 
+        products = self.env['product.product']
         sale_order = self.env["sale.order"].browse(res_ids)
-        lines_ids = sale_order.order_line.filtered(lambda s: s.product_id and s.product_id.purchase_ok and s.product_id.seller_ids)
-        res['lines_ids'] = [(6, 0, lines_ids.ids)]
+        products_all = self._get_products(sale_order)
+        for product_id, quantity in products_all.items():
+            if product_id.purchase_ok and product_id.seller_ids:
+                products |= product_id
 
+        res['product_ids'] = [(6, 0, products.ids)]
         return res
+
+    def _get_products(self, order_id):
+        lines = {}
+        for line in order_id.order_line:
+            if line.product_id in lines:
+                lines[line.product_id] += line.product_uom_qty
+            else:
+                lines[line.product_id] = line.product_uom_qty
+            if line.product_id.bom_count:
+                boms = self.env['mrp.bom'].search(['|', ('product_id', '=', line.product_id.id), '&', ('product_id', '=', False), ('product_tmpl_id', '=', line.product_id.product_tmpl_id.id)])
+                for bom in boms:
+                    components = self.env['report.mrp.report_bom_structure']._get_pdf_line(bom.id, line.product_id, line.product_uom_qty, child_bom_ids=[], unfolded=True)
+                    if components.get('lines'):
+                        for component in components['lines']:
+                            product_component = self.env['product.product'].browse(component['prod_id'])
+                            if product_component in lines:
+                                lines[product_component] += line.product_uom_qty
+                            else:
+                                lines[product_component] = line.product_uom_qty
+
+        return lines
 
     def generate(self):
         res_ids = self._context.get('active_ids')
         sale_order = self.env["sale.order"].browse(res_ids)
+        products_all = self._get_products(sale_order)
         for record in self:
             # create requisition
-            if len(record.lines_ids):
+            if len(record.product_ids):
                 purchase_vals = {
                     'type_id': self.env.ref('purchase_requisition.type_multi').id,
                     'company_id': self.env.company.id,
@@ -33,11 +59,15 @@ class SaleRequisitionGenerate(models.TransientModel):
                     'sale_id': sale_order.id,
                 }
                 requisition = self.env["purchase.requisition"].create(purchase_vals)
-            for line in record.lines_ids:
+            for product in record.product_ids:
+                if products_all.get(product):
+                    qty = products_all[product]
+                else:
+                    qty = 1
                 line_vals = {
-                    'product_id': line.product_id.id,
-                    'product_qty': line.product_uom_qty,
-                    'product_uom_id': line.product_uom.id,
+                    'product_id': product.id,
+                    'product_qty': qty,
+                    'product_uom_id': product.uom_po_id.id,
                     'requisition_id': requisition.id,
                 }
                 self.env["purchase.requisition.line"].create(line_vals)
