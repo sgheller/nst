05759d6a5d5bdbd46b5a1d308cc5cbebc13ffccf
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2021-09-30 15:28:55 +0200

    [REQ-0000000141] Ajout zone Acheteur dans l’onglet achat du partenaire qui fera référence à un utilisateur Odoo

---
+++ phi_vendor_buyer/models/partner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    po_user_id = fields.Many2one('res.users', 'Vendor Buyer')
