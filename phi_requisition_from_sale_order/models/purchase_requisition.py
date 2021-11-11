657ac44b7565b1519b64897163b26ca92ac44fda
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2021-11-11 16:49:18 +0100

    [REQ-0000000298] Ajout d'articles multiples dans appel d'offre

--- phi_requisition_from_sale_order/models/purchase_requisition.py
+++ phi_requisition_from_sale_order/models/purchase_requisition.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class PurchaseRequisition(models.Model):
@@ -10,19 +10,52 @@ class PurchaseRequisition(models.Model):
     sale_id = fields.Many2one('sale.order', 'Sale Order')
     account_analytic_id = fields.Many2one('account.analytic.account', string='Compte Analytique')
 
+    def purchase_requisition_add_products(self):
+        compose_form = self.env.ref('phi_requisition_from_sale_order.view_purchase_requisition_add_products', raise_if_not_found=False)
+
+        return {
+            'name': _('Add Products'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'purchase.requisition.add.products',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+        }
+
     def generate_default_purchase_orders(self):
         self.ensure_one()
-        for line in self.line_ids:
-            vendors = line.product_id.seller_ids.mapped('name')
-            for vendor in vendors:
-                purchase_order = self.purchase_ids.filtered(lambda s: s.partner_id == vendor)
-                if not purchase_order:
-                    purchase_order = self.env['purchase.order'].create({
-                        'partner_id': vendor.id,
-                        'requisition_id': self.id,
-                        'date_order': self.ordering_date or self.write_date,
-                        'account_analytic_id': self.account_analytic_id.id,
+        if not self.vendor_id:
+            for line in self.line_ids:
+                vendors = line.product_id.seller_ids.mapped('name')
+                for vendor in vendors:
+                    purchase_order = self.purchase_ids.filtered(lambda s: s.partner_id == vendor)
+                    if not purchase_order:
+                        purchase_order = self.env['purchase.order'].create({
+                            'partner_id': vendor.id,
+                            'requisition_id': self.id,
+                            'date_order': self.ordering_date or self.write_date,
+                            'account_analytic_id': self.account_analytic_id.id,
+                        })
+                    self.env['purchase.order.line'].create({
+                        'order_id': purchase_order.id,
+                        'product_id': line.product_id.id,
+                        'product_qty': line.product_qty,
+                        'product_uom': line.product_uom_id.id,
+                        'date_planned': line.schedule_date or self.schedule_date or purchase_order.date_order,
                     })
+        else:
+            vendor = self.vendor_id
+            purchase_order = self.purchase_ids.filtered(lambda s: s.partner_id == vendor)
+            if not purchase_order:
+                purchase_order = self.env['purchase.order'].create({
+                    'partner_id': vendor.id,
+                    'requisition_id': self.id,
+                    'date_order': self.ordering_date or self.write_date,
+                    'account_analytic_id': self.account_analytic_id.id,
+                })
+            for line in self.line_ids:
                 self.env['purchase.order.line'].create({
                     'order_id': purchase_order.id,
                     'product_id': line.product_id.id,
