8a9fce1c2f28bdab6b5d0ffdc174aad059bf58bc
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2021-11-11 11:38:59 +0100

    [REQ-0000000310] Possibilité d’ajouter une case sur les bons de commande ou demande de prix indiquant le nombre de plan envoyé dans le mail

--- nexans_specifique/__manifest__.py
+++ nexans_specifique/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "Phidias",
     'website': "http://www.phidias.fr",
     'category': 'Uncategorized',
-    'version': '14.0.0.14',
+    'version': '14.0.0.15',
 
     # any module necessary for this one to work correctly
     'depends': [
